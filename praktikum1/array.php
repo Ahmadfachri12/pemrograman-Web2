e5d6d75d316f7dddd967b1a6b4037e3fe065acfe
Ahmadfachri12 <fahriahmd111@gmail.com>
2024-02-19 15:32:04 +0700

    upload praktiukum 1

---
+++ praktikum1/array.php
@@ -0,0 +1,24 @@
+<?php 
+$fruits = ["Mango", "Banana", "Melon", "Orange"];
+$alamat = "Cilebut";
+
+echo $fruits[1];
+echo "<br> ";
+echo "Jumlah buah = " . count($fruits);
+
+echo "<ol>";
+foreach ($fruits as $fruit) {
+    echo "<li>Buah " . $fruit . "</li>";
+}
+echo "</ol>";
+
+$fruits[] = "Avocado";
+$fruits[2] = "Stawberry";
+
+echo "<ol>";
+foreach ($fruits as $i => $v) {
+    echo "<li>Buah index ke-" . $i . " = " .$v . "</li>";
+}
+echo "</ol>";
+
+?>
---
+++ praktikum1/index.php
@@ -0,0 +1,12 @@
+<?php 
+// Mendefinisikan Variabel
+$nama = "Ahmad Fachri";
+$umur = 18;
+$berat = 50;
+
+echo $nama;
+echo "<br>Umur " . $nama . " Adalah "  . $umur . " Tahun ";
+echo "<br>Berat Badan = " . $berat . " kg";
+
+
+?>
