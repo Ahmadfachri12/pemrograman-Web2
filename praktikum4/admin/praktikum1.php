04018eaa78844df478a44ce042c15bf1e0e67d67
Ahmadfachri12 <fahriahmd111@gmail.com>
2024-03-16 10:49:58 +0700

    praktikum 4

---
+++ praktikum4/admin/praktikum1.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'navbar.html';
+require_once 'sidebar.html';
+?>
+
+<h2>Praktikum 1</h2>
+<?php 
+$fruits = ["Mango", "Banana", "Melon", "Orange"];
+$alamat = "Cilebut";
+
+echo $fruits[1];
+echo "<br> ";
+echo "Jumlah buah = " . count($fruits);
+
+echo "<ol>";
+foreach ($fruits as $fruit) {
+    echo "<li>Buah " . $fruit . "</li>";
+}
+echo "</ol>";
+
+$fruits[] = "Avocado";
+$fruits[2] = "Stawberry";
+
+echo "<ol>";
+foreach ($fruits as $i => $v) {
+    echo "<li>Buah index ke-" . $i . " = " .$v . "</li>";
+}
+echo "</ol>";
+
+?>
+
+<?php
+require_once 'footer.html';
+
+?>
