92ac30a86f7d7d8ab0548b3a712c1c25f4fcd772
Ahmadfachri12 <fahriahmd111@gmail.com>
2024-03-18 15:43:58 +0700

    apload pratikum 5

---
+++ praktikum5/persegi_panjang.php
@@ -0,0 +1,26 @@
+<?php
+
+class PersegiPanjang {
+    // Property
+    public $panjang;
+    public $lebar;
+
+    // Method
+    function __construct($p, $l){
+        $this->panjang = $p;
+        $this->lebar = $l;
+    }
+
+    function getLuas(){
+        return $this->panjang * $this->lebar;
+    }
+
+    function getKeliling(){
+        return 2 * ($this->panjang + $this->lebar);
+    }
+}
+
+// Instance Object
+$persegi_panjang1 = new PersegiPanjang(10, 5);
+echo "<br>Luas PersegiPanjang = " . $persegi_panjang1->getLuas();
+echo "<br>Keliling PergegiPanjang  = " . $persegi_panjang1->getKeliling();
