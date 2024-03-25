d4be447ce63d7a08de2ae8040f23db4f60375295
Ahmadfachri12 <fahriahmd111@gmail.com>
2024-03-25 15:33:38 +0700

    belajar koneksi php dan mysql

---
+++ praktikum6/koneksi.php
@@ -0,0 +1,17 @@
+<?php
+$host ='localhost';
+$database ='db_puskesmas2';
+$user ='root';
+$pass ='';
+$charset ='utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$database;charset=$charset";
+
+$opt = [
+    PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES=>false,
+];
+
+$dbh = new PDO($dsn, $user, $pass, $opt);
+?>
